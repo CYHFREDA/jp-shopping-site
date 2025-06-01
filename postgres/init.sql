e61ef0e51fbc8b12e75c5013aa4dd6ee5b1392fa
freda <inulifegogo@gmail.com>
2025-06-01 10:35:44 +0800

    06/01/2025 10:35:44

--- postgres/init.sql
+++ postgres/init.sql
@@ -18,11 +18,11 @@ COMMENT ON COLUMN orders.paid_at IS '付款時間（可為空）';
 
 -------------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS products (
-  id SERIAL PRIMARY KEY, -- 商品 ID，主鍵，自動增加
+  id SERIAL PRIMARY KEY,  -- 商品 ID，主鍵，自動增加
   name TEXT NOT NULL, -- 商品名稱，必填
   price INTEGER NOT NULL, -- 商品價格，必填，整數
-  description TEXT, -- 商品描述，選填
-  image_url TEXT, -- 商品圖片網址，選填
+  description TEXT NOT NULL, -- 商品描述，必填
+  image_url TEXT NOT NULL, -- 商品圖片網址，必填
   created_at TIMESTAMP DEFAULT NOW(), -- 商品建立時間，預設為當下時間
   category VARCHAR(255) -- 商品分類，可多分類用「#」分隔
 );
@@ -36,30 +36,32 @@ COMMENT ON COLUMN products.created_at IS '商品建立時間，預設為當下�
 COMMENT ON COLUMN products.category IS '商品分類，可多分類用「#」分隔';
 -------------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS customers (
-  customer_id SERIAL PRIMARY KEY,            -- 客戶 ID，自動增加
-  username VARCHAR(50) UNIQUE NOT NULL,      -- 使用者名稱，唯一且必填
-  name VARCHAR(50) NOT NULL,                 -- 使用者姓名，必填
-  email VARCHAR(100),                        -- 電子郵件，可選填
-  phone VARCHAR(20),                         -- 電話，可選填
-  password VARCHAR(100) NOT NULL,            -- 密碼（bcrypt 雜湊後的結果），必填
-  created_at TIMESTAMP DEFAULT NOW()         -- 建立時間，預設當下
+  customer_id SERIAL PRIMARY KEY,          -- 客戶 ID，自動增加
+  username VARCHAR(50) UNIQUE NOT NULL,    -- 使用者名稱，唯一且必填
+  name VARCHAR(50) NOT NULL,               -- 使用者姓名，必填
+  email VARCHAR(100) NOT NULL,             -- 電子郵件，必填
+  phone VARCHAR(20) NOT NULL,              -- 電話，必填
+  password VARCHAR(100) NOT NULL,          -- 密碼（bcrypt 雜湊後的結果），必填
+  address VARCHAR(255)  NOT NULL,          -- 地址，必填
+  created_at TIMESTAMP DEFAULT NOW()       -- 建立時間，預設當下
 );
 
 COMMENT ON COLUMN customers.customer_id IS '客戶 ID，自動增加';
 COMMENT ON COLUMN customers.username IS '使用者名稱，唯一且必填';
 COMMENT ON COLUMN customers.name IS '使用者姓名，必填';
-COMMENT ON COLUMN customers.email IS '電子郵件，可選填';
-COMMENT ON COLUMN customers.phone IS '電話，可選填';
+COMMENT ON COLUMN customers.email IS '電子郵件，必填';
+COMMENT ON COLUMN customers.phone IS '電話，必填';
 COMMENT ON COLUMN customers.password IS '密碼（bcrypt 雜湊後的結果），必填';
+COMMENT ON COLUMN customers.address IS '地址，必填';
 COMMENT ON COLUMN customers.created_at IS '建立時間，預設當下';
 -------------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS shipments (
   shipment_id SERIAL PRIMARY KEY,        -- 出貨單 ID，自動增加
-  order_id VARCHAR(50),                   -- 對應的訂單編號
-  recipient_name VARCHAR(50),             -- 收件人姓名
-  address VARCHAR(200),                   -- 收件地址
-  status VARCHAR(20),                     -- 出貨狀態（例：pending、shipped...）
-  created_at TIMESTAMP DEFAULT NOW()      -- 建立時間
+  order_id VARCHAR(50) NOT NULL,                  -- 對應的訂單編號，必填
+  recipient_name VARCHAR(50) NOT NULL,            -- 收件人姓名，必填
+  address VARCHAR(200) NOT NULL,                  -- 收件地址，必填
+  status VARCHAR(20) NOT NULL,                    -- 出貨狀態（例：pending、shipped...），必填
+  created_at TIMESTAMP DEFAULT NOW()     -- 建立時間
 );
 COMMENT ON COLUMN shipments.shipment_id IS '出貨單 ID，自動增加';
 COMMENT ON COLUMN shipments.order_id IS '對應的訂單編號';
