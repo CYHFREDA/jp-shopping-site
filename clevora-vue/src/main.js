e42672ad48498dc0000eabc1c2cf3b3d57dccf5c
freda <freda@5gg.win>
2025-06-03 16:27:55 +0800

    06/03/2025 16:27:55

--- clevora-vue/src/main.js
+++ clevora-vue/src/main.js
@@ -6,7 +6,7 @@ import router from './router';
 import axios from 'axios';
 
 // 設定 axios 的基礎 URL
-axios.defaults.baseURL = 'http://localhost:3000';  // 請根據您的後端 API 地址修改
+axios.defaults.baseURL = 'http://backend-api:80';  // 已根據您的後端 API 地址修改
 
 // 引入全局樣式
 import './assets/main.css';
